33526a43cbb64cdf9dce24b62f1f7fe240f8c1e7
Ms.Joseph <126281712+jenni0013@users.noreply.github.com>
2024-11-06 20:23:38 +0530

    Update pyth.py

--- pyth.py
+++ pyth.py
@@ -1,6 +1,1 @@
-#this is a  comment
-print("this isn't a comment")
-#this is another statement
-print("this is another statement.")
-print("this is a statement , ya like you didnt know it already, hehehe")
-print("ya, im done , i mean almost, HOPEFULLY IM DONE with this quiickly enough")
+
